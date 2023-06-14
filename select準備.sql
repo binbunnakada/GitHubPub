eeff0d290d6b01389836f8089d308d1212aab943
nakada_ankle <nakada_ankle@lookup.co.jp>
2023-06-14 10:22:20 +0900

    #wrk を削除

--- select準備.sql
+++ select準備.sql
@@ -1,13 +1,14 @@
 
--- ïœêîêÈåæ
 
 
-	--DROP TABLE #wrk
-
+	SELECT TopickDay
+    ,MAX(CASE WHEN TopickCode = '4536.T' THEN EndPrice ELSE NULL END) AS '4536.T'
+    ,MAX(CASE WHEN TopickCode = '6432.T' THEN EndPrice ELSE NULL END) AS '6432.T'
+    ,MAX(CASE WHEN TopickCode = '6656.T' THEN EndPrice ELSE NULL END) AS '6656.T'
+	FROM (
 		SELECT convert(varchar, TopickDay, 23) TopickDay
 		,tmp2.TopickCode
 		,EndPrice
-		INTO #wrk
 		FROM (
 			SELECT TOP(3)  TopickCode, CNT  
 			, maxPrice
@@ -27,23 +28,10 @@
 		) tmp2
 		,	dDaylyPrice
 		WHERE tmp2.TopickCode = dDaylyPrice.TopickCode
-		ORDER BY dDaylyPrice.TopickDay, dDaylyPrice.TopickCode
+	)tmp3
+	GROUP BY TopickDay
+	ORDER BY TopickDay;
 
-
-
-		SELECT TopickDay
-       ,MAX(CASE WHEN TopickCode = '4536.T' THEN EndPrice ELSE NULL END) AS '4536.T'
-       ,MAX(CASE WHEN TopickCode = '6432.T' THEN EndPrice ELSE NULL END) AS '6432.T'
-       ,MAX(CASE WHEN TopickCode = '6656.T' THEN EndPrice ELSE NULL END) AS '6656.T'
-		FROM #wrk
-		GROUP BY TopickDay
-		ORDER BY TopickDay;
-
-
-
-	--select * from #wrk
-
-	DROP TABLE #wrk
 
 
 
